73fdc5ae760ac35f1facaa7207885fa47be25903
Tommaso <>
2025-03-05 13:04:28 +0100

    restored z3 from binary

--- cmake/Z3.cmake
+++ cmake/Z3.cmake
@@ -22,61 +22,107 @@ macro(obtain_z3)
     set(Z3_INCLUDE_DIR ${Z3_INCLUDE_DIR})
   else()
     # Pull in a specific version of Z3 and link against it.
-    set(Z3_VERSION "4.13.0")
-    message(STATUS "Fetching Z3 version ${Z3_VERSION}...")
+    set(P4TOOLS_Z3_VERSION "4.14.0")
+    message("Fetching Z3 version ${P4TOOLS_Z3_VERSION} for P4Tools...")
 
-    # Unity builds do not work for Protobuf...
-    set(CMAKE_UNITY_BUILD_PREV ${CMAKE_UNITY_BUILD})
-    set(CMAKE_UNITY_BUILD OFF)
+
+    # Pull in a specific version of Z3 and link against it.
+    # set(Z3_VERSION "4.14.0")
+    # message(STATUS "Fetching Z3 version ${Z3_VERSION}...")
+
+    # Determine platform to fetch pre-built Z3
+    if (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+      set(Z3_ARCH "x64")
+      if (APPLE)
+        set(Z3_PLATFORM_SUFFIX "osx-13.7.2")
+        set(Z3_ZIP_HASH "00")
+      elseif (UNIX)
+        set(Z3_PLATFORM_SUFFIX "glibc-2.35")
+        set(Z3_ZIP_HASH "d83e50e3cc9fcf86efa55f8a4d43f37ddc5af4214acfa26066bb340e2047b582")
+      else()
+        message(FATAL_ERROR "Unsupported system platform")
+      endif()
+    elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm64")
+      set(Z3_ARCH "arm64")
+      if (APPLE)
+        set(Z3_PLATFORM_SUFFIX "osx-13.7.2")
+        set(Z3_ZIP_HASH "00")
+      else()
+        message(FATAL_ERROR "Unsupported system platform")
+      endif()
+    else()
+      message(FATAL_ERROR "Unsupported system processor")
+    endif()
+
     # Print out download state while setting up Z3.
     set(FETCHCONTENT_QUIET_PREV ${FETCHCONTENT_QUIET})
     set(FETCHCONTENT_QUIET OFF)
-
-    set(Z3_BUILD_LIBZ3_SHARED OFF CACHE BOOL "Build libz3 as a shared library if true, otherwise build a static library")
-    set(Z3_INCLUDE_GIT_HASH OFF CACHE BOOL "Include git hash in version output")
-    set(Z3_INCLUDE_GIT_DESCRIBE OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_TEST_EXECUTABLES OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_SAVE_CLANG_OPTIMIZATION_RECORDS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_ENABLE_TRACING_FOR_NON_DEBUG OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_ENABLE_EXAMPLE_TARGETS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_DOTNET_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_PYTHON_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_JAVA_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_JULIA_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_DOCUMENTATION OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_ALWAYS_BUILD_DOCS OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_API_LOG_SYNC OFF CACHE BOOL "Include git describe output in version output")
-    set(Z3_BUILD_EXECUTABLE OFF CACHE BOOL "Include git describe output in version output")
     fetchcontent_declare(
       z3
-      GIT_REPOSITORY https://github.com/Z3Prover/z3.git
-      GIT_TAG z3-4.13.3 # 4.13.0
+      URL https://github.com/Z3Prover/z3/releases/download/z3-${P4TOOLS_Z3_VERSION}/z3-${P4TOOLS_Z3_VERSION}-${Z3_ARCH}-${Z3_PLATFORM_SUFFIX}.zip
+      URL_HASH SHA256=${Z3_ZIP_HASH}
+      USES_TERMINAL_DOWNLOAD TRUE
       GIT_PROGRESS TRUE
-      GIT_SHALLOW TRUE
-      # We need to patch because the Z3 CMakeLists.txt also adds an uinstall target.
-      # This leads to a namespace clash.
-      PATCH_COMMAND
-        git apply ${P4C_SOURCE_DIR}/cmake/z3.patch || git apply
-        ${P4C_SOURCE_DIR}/cmake/z3.patch -R --check && echo
-        "Patch does not apply because the patch was already applied."
     )
-    fetchcontent_makeavailable_but_exclude_install(z3)
-
-    # Suppress warnings for all Z3 targets.
-    get_all_targets(Z3_BUILD_TARGETS ${z3_SOURCE_DIR})
-    foreach(target ${Z3_BUILD_TARGETS})
-        # Do not suppress warnings for Z3 library targets that are aliased.
-        get_target_property(target_type ${target} TYPE)
-        if (NOT ${target_type} STREQUAL "INTERFACE_LIBRARY" AND NOT ${target_type} STREQUAL "UTILITY")
-          target_compile_options(${target} PRIVATE "-Wno-error" "-w")
-          # Z3 does not add its own include directories for compilation, which can lead to conflicts.
-          target_include_directories(${target} BEFORE PRIVATE ${z3_SOURCE_DIR}/src)
-      endif()
-    endforeach()
-
-    # Reset temporary variable modifications.
-    set(CMAKE_UNITY_BUILD ${CMAKE_UNITY_BUILD_PREV})
+    fetchcontent_makeavailable(z3)
     set(FETCHCONTENT_QUIET ${FETCHCONTENT_QUIET_PREV})
+    message("Done with setting up Z3 for P4Tools.")
+
+    
+    
+
+    # # Unity builds do not work for Protobuf...
+    # set(CMAKE_UNITY_BUILD_PREV ${CMAKE_UNITY_BUILD})
+    # set(CMAKE_UNITY_BUILD OFF)
+    # # Print out download state while setting up Z3.
+    # set(FETCHCONTENT_QUIET_PREV ${FETCHCONTENT_QUIET})
+    # set(FETCHCONTENT_QUIET OFF)
+
+    # set(Z3_BUILD_LIBZ3_SHARED OFF CACHE BOOL "Build libz3 as a shared library if true, otherwise build a static library")
+    # set(Z3_INCLUDE_GIT_HASH OFF CACHE BOOL "Include git hash in version output")
+    # set(Z3_INCLUDE_GIT_DESCRIBE OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_TEST_EXECUTABLES OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_SAVE_CLANG_OPTIMIZATION_RECORDS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_ENABLE_TRACING_FOR_NON_DEBUG OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_ENABLE_EXAMPLE_TARGETS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_DOTNET_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_PYTHON_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_JAVA_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_JULIA_BINDINGS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_DOCUMENTATION OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_ALWAYS_BUILD_DOCS OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_API_LOG_SYNC OFF CACHE BOOL "Include git describe output in version output")
+    # set(Z3_BUILD_EXECUTABLE OFF CACHE BOOL "Include git describe output in version output")
+    # fetchcontent_declare(
+    #   z3
+    #   GIT_REPOSITORY https://github.com/Z3Prover/z3.git
+    #   GIT_TAG z3-4.13.3 # 4.13.0
+    #   GIT_PROGRESS TRUE
+    #   GIT_SHALLOW TRUE
+    #   # We need to patch because the Z3 CMakeLists.txt also adds an uinstall target.
+    #   # This leads to a namespace clash.
+    #   PATCH_COMMAND
+    #     git apply ${P4C_SOURCE_DIR}/cmake/z3.patch || git apply
+    #     ${P4C_SOURCE_DIR}/cmake/z3.patch -R --check && echo
+    #     "Patch does not apply because the patch was already applied."
+    # )
+    # fetchcontent_makeavailable_but_exclude_install(z3)
+
+    # # Suppress warnings for all Z3 targets.
+    # get_all_targets(Z3_BUILD_TARGETS ${z3_SOURCE_DIR})
+    # foreach(target ${Z3_BUILD_TARGETS})
+    #     # Do not suppress warnings for Z3 library targets that are aliased.
+    #     get_target_property(target_type ${target} TYPE)
+    #     if (NOT ${target_type} STREQUAL "INTERFACE_LIBRARY" AND NOT ${target_type} STREQUAL "UTILITY")
+    #       target_compile_options(${target} PRIVATE "-Wno-error" "-w")
+    #       # Z3 does not add its own include directories for compilation, which can lead to conflicts.
+    #       target_include_directories(${target} BEFORE PRIVATE ${z3_SOURCE_DIR}/src)
+    #   endif()
+    # endforeach()
+
+    # # Reset temporary variable modifications.
+    # set(CMAKE_UNITY_BUILD ${CMAKE_UNITY_BUILD_PREV})
+    # set(FETCHCONTENT_QUIET ${FETCHCONTENT_QUIET_PREV})
 
     # Other projects may also pull in Z3.
     # We have to make sure we only include our local version.
